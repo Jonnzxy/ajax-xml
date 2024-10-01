7f959ecf85ed345188c01e2ed543ebf16ddb365e
Jonathan Rafael <161335155+Jonnzxy@users.noreply.github.com>
2024-10-01 11:34:33 -0300

    js

--- js.js
+++ js.js
@@ -1,53 +1,48 @@
-// endereço
-const xmlURL = 'sistemap.xml';
+// Endereço do XML
+const xmlURL = 'sitemap-news.xml';
 
+// Função para buscar o XML
 function buscarXML() {
     fetch(xmlURL)
-        .then(response => response.text())
-        .then(data => {
-            //converter texto em DOM
-            let parser = new DOMparser();
-            let xml = parser.parseFromString(data,
-                "application/xml")
-                // extrair os dados desejados
-                let noticias = xml.getElementByTagName("url");
-                //elemento para exibir as noticias 
-                let manchetesCotainer = document.getElementById("manchetes");
-                manchetesContainer.innerHTML = ""; //limpa a div
+    .then(response => response.text())
+    .then(data => {
+        // Converter o texto em DOM
+        let parser = new DOMParser();
+        let xml = parser.parseFromString(data, "application/xml");
+        
+        // Extrair os dados desejados (exemplo URL das notícias)
+        let noticias = xml.getElementsByTagName("url");
+        // Elemento onde vou exibir as notícias
+        let manchetesContainer = document.getElementById("manchetes");
+        manchetesContainer.innerHTML = ""; // Limpa o elemento
 
-                //vamos percorrer a lista usando for
-                for (let i = 0; i < noticias.length; i++) {
-                let doc = noticias[i].getElementsByTagName("loc")[0].textContent;
-                ("loc")[0].textcontent;
+        // Percorrer as notícias usando um for
+        for (let i = 0; i < noticias.length; i++) {
+            let loc = noticias[i].getElementsByTagName("loc")[0].textContent;
 
-                //extrai a data de public.
+            // Verificação para data de publicação sem o namespace "news:"
+            let data_publi_element = noticias[i].getElementsByTagName("publication_date")[0];
+            let data_publi = data_publi_element ? data_publi_element.textContent : 'Data não disponível';
 
-                let data_publi_element = noticias[i].getElementByTagName ("publication_data")[0];
-                let data_publi = data_publi_element ?
-                data_publi_element.textcontent : 'data indisp';
+            // Verificação para o título sem o namespace "news:"
+            let titulo_element = noticias[i].getElementsByTagName("title")[0];
+            let titulo = titulo_element ? titulo_element.textContent : 'Título não disponível';
 
-                //verifica o título
-                let titulo_element = noticias[i].
-                getElementByTagName("title")[0];
-                let titulo = titulo_element ? titulo_element.
-                textcontent : 'titulo_indisponivel';
-
-                //interpolação de variaveis
-                let montadiv = `
-                <div class = 'noticias'> 
-                    <h2>$ {titulo} </h2>
-                    <p> publicado em> $ {datapubli}</p>
-                    <a href='${loc}' target='_blank'>
-                    leia mais </a><hr>
-                `;
-                manchetesContainer.innerHTML += montadiv;
-
-
-
-
-                }
+            // Usar crases para interpolação de variáveis
+            let montadiv = `
+                <div class='noticias'>
+                    <h2>${titulo}</h2>
+                    <p>Publicado em: ${data_publi}</p>
+                    <a href='${loc}' target='_blank'>Leia mais</a>
+                </div>
+                <hr/>
+            `;
+            manchetesContainer.innerHTML += montadiv;
+        }
     }).catch(error => {
-        console.error('erro ao carregar o xml', error);
+        console.error('Erro ao carregar o XML:', error);
     });
 }
+
+// Chama a função quando a página for carregada
 window.onload = buscarXML;
